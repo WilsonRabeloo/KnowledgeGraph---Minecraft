71c5a07904ae38061289c62db2f6773685b4406f
vicentelc06 <evnct3113@gmail.com>
2026-07-07 18:03:27 -0300

    adicionei muitos itens e relações

--- main.py
+++ main.py
@@ -13,6 +13,83 @@ triples = {
     ("pedregulho", "crafta", "picareta_de_pedra"),
     ("graveto", "crafta", "picareta_de_pedra"),
     ("pedregulho", "crafta", "fornalha"),
+    ("ovelha", "dropa", "la"),
+    ("ovelha", "dropa", "carne_de_ovelha"),
+    ("carne_de_ovelha", "cozinha_em", "carne_de_ovelha_cozida"),
+    ("la", "crafta", "cama"),
+    ("tabua", "crafta", "cama"),
+    ("vaca", "dropa", "couro"),
+    ("vaca", "dropa", "bife_cru"),
+    ("bife_cru", "cozinha_em", "file"),
+    ("couro", "crafta", "capacete_de_couro"),
+    ("couro", "crafta", "peitoral_de_couro"),
+    ("couro", "crafta", "calca_de_couro"),
+    ("couro", "crafta", "bota_de_couro"),
+    ("minerio_de_carvao", "e_quebrado_em", "carvao"),
+    ("carvao", "crafta", "tocha"),
+    ("graveto", "crafta", "tocha"),
+    ("carvao", "e_usado_em", "fornalha"),
+    ("picareta_de_pedra", "quebra", "minerio_de_ferro"),
+    ("picareta_de_madeira", "quebra", "minerio_de_carvao"),
+    ("picareta_de_pedra", "quebra", "minerio_de_carvao"),
+    ("minerio_de_ferro", "e_quebrado_em", "ferro_bruto"),
+    ("ferro_bruto", "e_derretido_em", "barra_de_ferro"),
+    ("picareta_de_pedra", "quebra", "pedra"),
+    ("zumbi", "dropa", "carne_podre"),
+    ("aranha", "dropa", "olho_de_aranha"),
+    ("aranha", "dropa", "linha"),
+    ("linha", "crafta", "vara_de_pesca"),
+    ("graveto", "crafta", "vara_de_pesca"),
+    ("areia", "e_derretido_em", "vidro"),
+    ("vidro", "crafta", "vidraca"),
+    ("graveto", "crafta", "picareta_de_ferro"),
+    ("barra_de_ferro", "crafta", "picareta_de_ferro"),
+    ("barra_de_ferro", "crafta", "capacete_de_ferro"),
+    ("barra_de_ferro", "crafta", "peitoral_de_ferro"),
+    ("barra_de_ferro", "crafta", "calca_de_ferro"),
+    ("barra_de_ferro", "crafta", "bota_de_ferro"),
+    ("picareta_de_ferro", "quebra", "pedra"),
+    ("picareta_de_ferro", "quebra", "minerio_de_carvao"),
+    ("picareta_de_ferro", "quebra", "minerio_de_ferro"),
+    ("picareta_de_ferro", "quebra", "minerio_de_diamante"),
+    ("minerio_de_diamante", "e_quebrado_em", "diamante"),
+    ("tabua", "crafta", "pa_de_madeira"),
+    ("tabua", "crafta", "machado_de_madeira"),
+    ("tabua", "crafta", "enxada_de_madeira"),
+    ("graveto", "crafta", "pa_de_madeira"),
+    ("graveto", "crafta", "enxada_de_madeira"),
+    ("graveto", "crafta", "machado_de_madeira"),
+    ("pedregulho", "crafta", "espada_de_pedra"),
+    ("pedregulho", "crafta", "pa_de_pedra"),
+    ("pedregulho", "crafta", "enxada_de_pedra"),
+    ("pedregulho", "crafta", "machado_de_pedra"),
+    ("graveto", "crafta", "espada_de_pedra"),
+    ("graveto", "crafta", "pa_de_pedra"),
+    ("graveto", "crafta", "enxada_de_pedra"),
+    ("graveto", "crafta", "machado_de_pedra"),
+    ("barra_de_ferro", "crafta", "espada_de_ferro"),
+    ("barra_de_ferro", "crafta", "pa_de_ferro"),
+    ("barra_de_ferro", "crafta", "enxada_de_ferro"),
+    ("barra_de_ferro", "crafta", "machado_de_ferro"),
+    ("graveto", "crafta", "espada_de_ferro"),
+    ("graveto", "crafta", "pa_de_ferro"),
+    ("graveto", "crafta", "enxada_de_ferro"),
+    ("graveto", "crafta", "machado_de_ferro"),
+    ("galinha", "dropa", "frango_cru"),
+    ("frango_cru", "cozinha_em", "frango_assado"),
+    ("galinha", "dropa", "pena"),
+    ("pena", "crafta", "flecha"),
+    ("graveto", "crafta", "flecha"),
+    ("galinha", "dropa", "ovo"),
+    ("flecha", "e_usado_em", "arco"),
+    ("graveto", "crafta", "arco"),
+    ("linha", "crafta", "arco"),
+    ("esqueleto", "dropa", "arco"),
+    ("esqueleto", "dropa", "osso"),
+    ("osso", "domestica", "lobo"),
+    ("creeper", "dropa", "polvora"),
+    ("porco", "dropa", "costela_de_porco_crua"),
+    ("costela_de_porco_crua", "cozinha_em", "costela_de_porco_assada")
 }
 
 df = pd.DataFrame(
@@ -52,6 +129,250 @@ nodes = {
     "fornalha": {
         "tipo": "bloco",
         "imagem": "icons/fornalha.png"
+    },
+    "ovelha": {
+       "tipo": "criatura",
+       "imagem": "icons/ovelha.png"
+    },
+    "la": {
+       "tipo": "bloco",
+       "imagem": "icons/la.png"
+    },
+    "cama": {
+       "tipo": "utilidade",
+       "imagem": "icons/cama.png"
+    },
+    "carne_de_ovelha": {
+       "tipo": "comida",
+       "imagem": "icons/carne_de_ovelha.png"
+    },
+    "carne_de_ovelha_cozida": {
+       "tipo": "comida",
+       "imagem": "icons/carne_de_ovelha_cozida.png"
+    },
+    "vaca": {
+        "tipo": "criatura",
+        "imagem": "icons/vaca.png"
+    },
+    "bife_cru": {
+        "tipo": "comida",
+        "imagem": "icons/bife_cru.png"
+    },
+    "file": {
+        "tipo": "comida",
+        "imagem": "icons/file.png"
+    },
+    "couro": {
+        "tipo": "recurso",
+        "imagem": "icons/couro.png"
+    },
+    "capacete_de_couro": {
+        "tipo": "armadura",
+        "imagem": "icons/capacete_de_couro.png"
+    },
+    "peitoral_de_couro": {
+        "tipo": "armadura",
+        "imagem": "icons/peitoral_de_couro.png"
+    },
+    "calca_de_couro": {
+        "tipo": "armadura",
+        "imagem": "icons/calca_de_couro.png"
+    },
+    "bota_de_couro": {
+        "tipo": "armadura",
+        "imagem": "icons/bota_de_couro.png"
+    },
+    "minerio_de_carvao":{
+        "tipo": "bloco",
+        "imagem": "icons/minerio_de_carvao.png"
+    },
+    "carvao": {
+        "tipo": "recurso",
+        "imagem": "icons/carvao.png"
+    },
+    "tocha": {
+        "tipo": "utilidade",
+        "imagem": "icons/tocha.png"
+    },
+    "minerio_de_ferro": {
+        "tipo": "bloco",
+        "imagem": "icons/minerio_de_ferro.png",
+    },
+    "ferro_bruto": {
+        "tipo": "recurso",
+        "imagem": "icons/ferro_bruto.png"
+    },
+    "barra_de_ferro": {
+        "tipo": "recurso",
+        "imagem": "icons/barra_de_ferro.png"
+    },
+    "areia": {
+        "tipo": "bloco",
+        "imagem": "icons/areia.png"
+    },
+    "vidro": {
+        "tipo": "bloco",
+        "imagem": "icons/vidro.png"
+    },
+    "vidraca": {
+        "tipo": "bloco",
+        "imagem": "icons/vidraca.png"
+    },
+    "zumbi": {
+        "tipo": "criatura",
+        "imagem": "icons/zumbi.png"
+    },
+    "carne_podre": {
+        "tipo": "comida",
+        "imagem": "icons/carne_podre.png"
+    },
+    "aranha": {
+        "tipo": "criatura",
+        "imagem": "icons/aranha.png"
+    },
+    "linha": {
+        "tipo": "recurso",
+        "imagem": "icons/linha.png"
+    },
+    "olho_de_aranha": {
+        "tipo": "recurso",
+        "imagem": "icons/olho_de_aranha.png"
+    },
+    "vara_de_pesca": {
+        "tipo": "ferramenta",
+        "imagem": "icons/vara_de_pesca.png"
+    },
+    "picareta_de_ferro": {
+        "tipo": "ferramenta",
+        "imagem": "icons/picareta_de_ferro.png"
+    },
+    "capacete_de_ferro": {
+        "tipo": "armadura",
+        "imagem": "icons/capacete_de_ferro.png"
+    },
+    "peitoral_de_ferro": {
+        "tipo": "armadura",
+        "imagem": "icons/peitoral_de_ferro.png"
+    },
+    "calca_de_ferro": {
+        "tipo": "armadura",
+        "imagem": "icons/calca_de_ferro.png",
+    },
+    "bota_de_ferro": {
+        "tipo": "armadura",
+        "imagem": "icons/bota_de_ferro.png"
+    },
+    "diamante": {
+        "tipo": "recurso",
+        "imagem": "icons/diamante.png"
+    },
+    "minerio_de_diamante": {
+        "tipo": "bloco",
+        "imagem": "icons/minerio_de_diamante.png"
+    },
+    "espada_de_pedra": {
+        "tipo": "ferramenta",
+        "imagem": "icons/espada_de_pedra.png"
+    },
+    "espada_de_ferro": {
+        "tipo": "ferramenta",
+        "imagem": "icons/espada_de_ferro.png",
+    },
+    "machado_de_madeira": {
+        "tipo": "ferramenta",
+        "imagem": "icons/machado_de_madeira.png"
+    },
+    "machado_de_pedra": {
+        "tipo": "ferramenta",
+        "imagem": "icons/machado_de_pedra.png"
+    },
+    "machado_de_ferro": {
+        "tipo": "ferramenta",
+        "imagem": "icons/machado_de_ferro.png"
+    },
+    "pa_de_madeira": {
+        "tipo": "ferramenta",
+        "imagem": "icons/pa_de_madeira.png"
+    },
+    "pa_de_pedra": {
+        "tipo": "ferramenta",
+        "imagem": "icons/pa_de_pedra.png"
+    },
+    "pa_de_ferro": {
+        "tipo": "ferramenta",
+        "imagem": "icons/pa_de_ferro.png"
+    },
+    "enxada_de_madeira": {
+        "tipo": "ferramenta",
+        "imagem": "icons/enxada_de_madeira.png"
+    },
+    "enxada_de_pedra": {
+        "tipo": "ferramenta",
+        "imagem": "icons/enxada_de_pedra.png"
+    },
+    "enxada_de_ferro": {
+        "tipo": "ferramenta",
+        "imagem": "icons/enxada_de_ferro.png"
+    },
+    "galinha": {
+        "tipo": "criatura",
+        "imagem": "icons/galinha.png"
+    },
+    "frango_cru": {
+        "tipo": "comida",
+        "imagem": "icons/frango_cru.png"
+    },
+    "frango_assado": {
+        "tipo": "comida",
+        "imagem": "icons/frango_assado.png"
+    },
+    "ovo": {
+        "tipo": "recurso",
+        "imagem": "icons/ovo.png"
+    },
+    "pena": {
+        "tipo": "recurso",
+        "imagem": "icons/pena.png"
+    },
+    "flecha": {
+        "tipo": "recurso",
+        "imagem": "icons/flecha.png"
+    },
+    "arco": {
+        "tipo": "ferramenta",
+        "imagem": "icons/arco.png"
+    },
+    "esqueleto": {
+        "tipo": "criatura",
+        "imagem": "icons/esqueleto.png"
+    },
+    "osso": {
+        "tipo": "recurso",
+        "imagem": "icons/osso.png"
+    },
+    "lobo": {
+        "tipo": "criatura",
+        "imagem": "icons/lobo.png"
+    },
+    "creeper": {
+        "tipo": "criatura",
+        "imagem": "icons/creeper.png"
+    },
+    "polvora": {
+        "tipo": "recurso",
+        "imagem": "icons/polvora.png"
+    },
+    "porco": {
+        "tipo": "criatura",
+        "imagem": "icons/porco.png"
+    },
+    "costela_de_porco_crua": {
+        "tipo": "comida",
+        "imagem": "icons/costela_de_porco_crua.png"
+    },
+    "costela_de_porco_assada": {
+        "tipo": "comida",
+        "imagem": "icons/costela_de_porco_assada.png"
     }
 }
 
@@ -155,5 +476,6 @@ while True:
 
     elif opcao == "4":
         print("encerrar processo")
+        salvar_grafo_html(nodes, triples)
         break
 
