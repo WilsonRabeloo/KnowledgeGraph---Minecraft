5e280f11a837cd6b877abe94b0c0eda1d4e12c37
WilsonRabeloo <rabelowilson002@gmail.com>
2026-07-08 16:12:05 -0300

    exclui um role

--- main.py
+++ main.py
@@ -377,46 +377,6 @@ nodes = {
 }
 
 
-#
-# G = nx.DiGraph()
-#
-# for node_id, attrs in nodes.items():
-#     G.add_node(node_id, **attrs)
-#
-# for head, relation, tail, in triples:
-#     G.add_edge(
-#         head,
-#         tail,
-#         relation=relation
-#     )
-#
-# net = Network(
-#     height="750px",
-#     width="100%",
-#     directed=True
-# )
-#
-# net.barnes_hut()
-#
-# for node_id, attrs in G.nodes(data=True):
-#     net.add_node(
-#         node_id,
-#         label=node_id,
-#         title=f"Tipo: {attrs["tipo"]}",
-#         shape="image",
-#         image=attrs["imagem"],
-#         size=30
-#     )
-#
-# for source, target, attrs in G.edges(data=True):
-#     net.add_edge(
-#         source,
-#         target,
-#         label=attrs["relation"],
-#     )
-#
-# net.write_html("minecraft_kg.html")
-
 def salvar_grafo_html(nodes, triples):
     G = nx.DiGraph()
 
